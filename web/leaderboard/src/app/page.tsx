000981d8de7110d9da63c3c817f345a035e6bd61
Maria Rykova <maria.nigay@yandex.ru>
2023-11-13 15:38:04 +0800

    add banner, table, footer

--- web/leaderboard/src/app/page.tsx
+++ web/leaderboard/src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import styles from "./page.module.sass";
+import styles from "./page.module.scss";
 import {
   Text,
   GeneralButton,
@@ -9,9 +11,79 @@ import {
   Display,
   CaretRight,
   ArrowTopRight,
+  ProviderIcon,
+  ArrowRight,
+  LoadingDots,
+  Socials,
 } from "@fluidity-money/surfing";
+import { LinkButton } from "@fluidity-money/surfing";
+
+export type IRow = React.HTMLAttributes<HTMLDivElement> & {
+  RowElement: React.FC<{ heading: string }>;
+};
+
+const DATA = [
+  {
+    rank: "1",
+    user: "user_1",
+    tx: "tx",
+    volume: 100,
+    yield: 10,
+  },
+  {
+    rank: "2",
+    user: "user_2",
+    tx: "tx",
+    volume: 100,
+    yield: 10,
+  },
+  {
+    rank: "3",
+    user: "user_3",
+    tx: "tx",
+    volume: 100,
+    yield: 10,
+  },
+];
 
 export default function Home() {
+  const [filterIndex, setFilterIndex] = useState(0);
+  const [data, setData] = useState(DATA);
+  const [loaded, setLoaded] = useState();
+  const [filteredHeadings, usefilteredHeadings] = useState([
+    { name: "RANK" },
+    { name: "USER" },
+    { name: "#TX" },
+    { name: "VOLUME (USD)" },
+    { name: "YIELD EARNED (USD)" },
+  ]);
+
+  const Table = () => {
+    return;
+  };
+
+  const Row = ({ RowElement, index, className }: IRow & { index: number }) => {
+    return (
+      <motion.tr
+        className={className}
+        key={`row-${index}`}
+        variants={{
+          enter: { opacity: [0, 1] },
+          ready: { opacity: 1 },
+          exit: { opacity: 0 },
+          transitioning: {
+            opacity: [0.75, 1, 0.75],
+            transition: { duration: 1.5, repeat: Infinity },
+          },
+        }}
+      >
+        {filteredHeadings.map(({ name }) => (
+          <RowElement heading={name} key={name} />
+        ))}
+      </motion.tr>
+    );
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.header}>
@@ -30,16 +102,173 @@ export default function Home() {
           </Heading>
         </div>
         <div className={styles.description}>
-          <Display color={"gray"} extraSmall>
+          <Display color={"gray"} size="xxs">
             Step into the Fluidity Arena!{" "}
           </Display>{" "}
-          <Text>
+          {/*<Heading className={styles.title}>
+            Step into the Fluidity Arena!{" "}
+          </Heading>*/}
+          <Text prominent>
             Compete in our Leaderboard Challenge by transacting with ƒluid
             Assets on-chain. Rise in ranks, earn your bragging rights, and claim
             exclusive rewards. Each week brings a new opportunity — Dive in,
             explore, and may the best ƒluider win! LEARN MORE
             <ArrowTopRight />
           </Text>
+        </div>
+      </div>
+
+      <div className={styles.ldb__banner}>
+        <Image
+          src="./prise.svg"
+          alt="Prise"
+          width={20}
+          height={24}
+          className={styles.ldb__banner_svg}
+        />
+
+        <Text>
+          Weekly Challenge: Top Volume Contributors Win Extra Rewards!
+        </Text>
+      </div>
+      <div className={styles.ldb__table}>
+        <div className={styles.ldb__table_header}>
+          <div>
+            <div className={styles.ldb__table_header_title}>
+              <Heading as="h3">Leaderboard</Heading>
+            </div>
+            <Text prominent>
+              This leaderboard shows your rank among other users
+              {filterIndex === 0 ? " per" : " for"}
+              &nbsp;
+              {filterIndex === 0 ? (
+                <span className={styles.ldb_table_time_filter}>24 HOURS</span>
+              ) : (
+                <span className={styles.ldb_table_time_filter}>ALL TIME</span>
+              )}
+              .
+            </Text>
+          </div>
+          <div className={styles.ldb__table_header_filters}>
+            <div>
+              <GeneralButton
+                type={filterIndex === 0 ? "secondary" : "transparent"}
+                handleClick={() => setFilterIndex(0)}
+              >
+                <Text code size="sm">
+                  24 HOURS
+                </Text>
+              </GeneralButton>
+              <GeneralButton
+                type={filterIndex === 1 ? "secondary" : "transparent"}
+                handleClick={() => setFilterIndex(1)}
+              >
+                <Text code size="sm">
+                  ALL TIME
+                </Text>
+              </GeneralButton>
+            </div>
+            <div>
+              <Text>SORT BY:</Text>
+              <GeneralButton
+                type="transparent"
+                //icon={<ArrowRight />}
+                onClick={() => console.log("rank")}
+              >
+                RANK
+              </GeneralButton>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.ldb__table_wrapper}>
+          {data.length === 0 ? (
+            !loaded ? (
+              <>
+                Fetching table data...
+                <div className="center-table-loading-anim loader-dots">
+                  {/*{showLoadingAnimation && <LoadingDots />}*/}
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="center-table-loading-anim loader-dots">
+                  <Text size="lg">No records found!</Text>
+                </div>
+              </>
+            )
+          ) : (
+            <table className={styles.ldb__table_content}>
+              {/* Table Headings */}
+              <thead>
+                <tr>
+                  {filteredHeadings.map((heading) => {
+                    return (
+                      <th key={heading.name}>
+                        <Text>{heading.name}</Text>
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+
+              {/* Table Body */}
+              {/*<AnimatePresence mode="wait" initial={false}>
+                <motion.tbody
+                  key={`page-${page}`}
+                  initial="enter"
+                  animate={
+                    isTransition.state === "idle" ? "enter" : "transitioning"
+                  }
+                  exit="exit"
+                  variants={{
+                    enter: {
+                      opacity: 1,
+                      transition: {
+                        when: "beforeChildren",
+                        staggerChildren: 0.05,
+                      },
+                    },
+                    exit: {
+                      opacity: 0,
+                      transition: {
+                        when: "afterChildren",
+                        staggerChildren: 0.05,
+                      },
+                    },
+                    transitioning: {},
+                  }}
+                >*/}
+              {/* Frozen Rows */}
+              {/*{frozenRows.map((row, i) => (
+                <Row index={i} key={i} {...renderRow(row)} />
+              ))}*/}
+              {/* Unfrozen Rows */}
+              {data
+                //.filter((_) => !freezeRow?.(_))
+                .map((row, i) => (
+                  <div key={i}>{row.rank}</div>
+                  //  <Row index={i} key={i} {...renderRow(row)} />
+                ))}
+              {/*</motion.tbody>
+              </AnimatePresence>*/}
+            </table>
+          )}
+        </div>
+      </div>
+
+      <Socials />
+      <div className={styles.ldb__footer}>
+        <div>
+          <Text as="p" size="xs">
+            Fluidity Money 2023
+          </Text>
+        </div>
+        <div className={styles.ldb__footer_btns}>
+          {/*<LinkButton size="xs">Terms</LinkButton>*/}
+          <Text size="xs">Terms</Text>
+          <Text size="xs">Privacy policy</Text>
+          <Text size="xs">© 2023 Fluidity Money. All Rights Reserved.</Text>
         </div>
       </div>
     </main>
